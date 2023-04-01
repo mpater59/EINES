5fad669be7bb8856fb2ed718d11fb871559b1080
mpater59 <mikolajpater123@gmail.com>
2023-04-01 15:11:17 +0200

    Modified routing_net.py

--- routing_net.py
+++ routing_net.py
@@ -42,8 +42,9 @@ class MyTopo(Topo):
 def runNetwork():
     # mininet topology initialization
     topo = MyTopo()
+    my_controller = RemoteController(ip='127.0.0.1', port=6653)
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, autoSetMacs=True, inNamespace=True,
-                  controller=partial(RemoteController, ip='127.0.0.1', port=6653))
+                  controller=my_controller)
     net.start()
 
     # Dumping host connections
